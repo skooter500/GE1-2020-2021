b04fcf001f408b40386eb2ca7dad35c54ef3a457
Bryan Duggan <skooter500@gmail.com>
2020-10-02 14:23:36 +0100

    bla

---
+++ Game Engines Examples/Assets/Wall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public int width = 20;
+    public int height = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int halfWidth = width / 2;
+
+        for(int i = -halfWidth ; i < halfWidth ; i ++)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(i, 0.5f, 0);
+            cube.GetComponent<Renderer>().material.color = 
+                Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+            cube.AddComponent<Rigidbody>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
