0ec75cc51f0772df7bdd24730eb6310545b1db8a
Bryan Duggan <skooter500@gmail.com>
2020-11-12 17:49:27 +0000

    bla

--- Game Engines Examples 2020/Assets/OctopusGenerator.cs
+++ Game Engines Examples 2020/Assets/OctopusGenerator.cs
@@ -13,15 +13,19 @@ public class OctopusGenerator : MonoBehaviour {
         
         for (int i = 0; i < numTentacles; i++)
         {
-            // Calculate the localPosition using Sin and Cos
-            // Transform the localPosition relative to the gameobject
-            // To make the position of the tentacle
-            // You can use TransformPoint or you can use quaternions!
-            // Instiantiate the tentacle
-            // Dont forget to set it's rotation!
-            // You can use Quaternion.AngleAxis for this
-            // Parent the tentacle to this gameobject
-            
+            Vector3 pos = new Vector3(
+                - Mathf.Sin(theta * i) * radius
+                , 0
+                , - Mathf.Cos(theta * i) * radius
+                );
+            pos = (transform.rotation * pos) + transform.position;
+            GameObject t = GameObject.Instantiate<GameObject>(
+                tentaclePrefab
+                , pos
+                , Quaternion.AngleAxis(theta * i * Mathf.Rad2Deg, Vector3.up)
+                );
+            //t.transform.GetChild(0).GetComponent<HeadRotator>().theta = Random.Range(0, Mathf.PI * 2.0f);
+            t.transform.parent = this.transform;
         }
     }
 	
