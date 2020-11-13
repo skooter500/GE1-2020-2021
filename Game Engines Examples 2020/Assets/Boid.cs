3bcdf6c12a2a837a0eb544711a24ca98829e9cb2
Bryan Duggan <skooter500@gmail.com>
2020-11-13 15:48:17 +0000

    physics 1

---
+++ Game Engines Examples 2020/Assets/Boid.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public Vector3 force;
+    public float mass  = 1.0f;
+    public float max = 5;
+
+    public Transform target;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + force);
+        
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + velocity);
+        
+    }
+
+    public Vector3 Seek(Transform target)
+    {
+        Vector3 desired = target.position - transform.position;
+        desired.Normalize();
+        desired *= max;
+
+        return desired - velocity;
+    }
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        force = Seek(target);
+
+        acceleration = force / mass;
+        velocity += acceleration * Time.deltaTime;
+        if (velocity.magnitude > 0)
+        {
+            transform.forward = velocity;
+            transform.position += velocity * Time.deltaTime;
+        }
+    }
+}
---
+++ Game Engines Examples 2020/Assets/UnityPhysicsCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityPhysicsCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public float time = 0;
+    public Rigidbody rb; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 0)
+        {
+            time += Time.deltaTime;
+        }
+        else
+        {
+            rb.isKinematic = true;
+            Debug.Log("Unity physics cube time += " + time);
+            Debug.Log("Unity physics cube velociy: " + rb.velocity);    
+        }
+    }
+}
