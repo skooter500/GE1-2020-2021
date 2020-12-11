5d49e905e22d4d60d3a7d5f2633144e9d97cb1a7
Bryan Duggan <skooter500@gmail.com>
2020-12-11 13:09:19 +0000

    bla

--- Game Engines Examples 2020/Assets/Sway.cs
+++ Game Engines Examples 2020/Assets/Sway.cs
@@ -4,30 +4,15 @@ using UnityEngine;
 
 public class Sway : MonoBehaviour
 {
-    public bool useJobSystem = false;  
-    public float angle = 30;
-    public float frequency = 1;
-
-    float theta = 0;
     // Start is called before the first frame update
     void Start()
     {
-        if (useJobSystem)
-        {
-            SwayJobManager.Instance.Add(this);
-        }
     }
 
     // Update is called once per frame
     
-
     void Update()
     {
-        if (! useJobSystem)
-        {
-            transform.rotation = Quaternion.AngleAxis(Mathf.Sin(theta) * angle, transform.right);
-            theta += Mathf.PI * 2.0f * Time.deltaTime * frequency;
-        }
     }
     
 }
